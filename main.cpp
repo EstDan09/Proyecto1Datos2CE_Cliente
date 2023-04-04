88225446609ab2030a9f8c824abe0d193969dc24
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2023-04-03 19:51:34 -0600

    1.7

--- main.cpp
+++ main.cpp
@@ -6,17 +6,71 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <string>
-#include "Game.h"
+#include "raylib.h"
+#define NUM_FRAMES 1 //recorte
 
 using namespace std;
 
-Game* game = nullptr;
 
 int main(int argc, const char * argv[])
 {
-    game = new Game();
+    //Variables para la ventana
+    //-----------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    //------------------------------
 
-    game->init("LamentoEnElTibet", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    //Arrancamos la ventana
+    //------------------------------
+    InitWindow(screenWidth, screenHeight, "Lamento en el Tibet");
+    //------------------------------
+
+    //Audio
+    //------------------------------
+    InitAudioDevice();
+    Sound fxButton = LoadSound("/home/esteban/CLionProjects/Proyecto1Datos2CE_Cliente/assets/one.wav");
+    //------------------------------
+
+    //Imagenes
+    //------------------------------
+    Image startButtonImage = LoadImage("/home/esteban/CLionProjects/Proyecto1Datos2CE_Cliente/assets/start.png");
+    ImageResize(&startButtonImage, 350, 200);
+    Texture2D startButton = LoadTextureFromImage(startButtonImage);
+
+    Image settingsButtonImage = LoadImage("/home/esteban/CLionProjects/Proyecto1Datos2CE_Cliente/assets/settings.png");
+    ImageResize(&settingsButtonImage, 100, 100);
+    Texture2D settingsButton = LoadTextureFromImage(settingsButtonImage);
+
+    //Image backgroundMenuImage = LoadImage("/home/esteban/CLionProjects/Proyecto1Datos2CE_Cliente/assets/bgMenu.png");
+    Texture2D backgroundImage = LoadTexture("/home/esteban/CLionProjects/Proyecto1Datos2CE_Cliente/assets/bgMenu.png");
+    //------------------------------
+
+    //Parametros del boton Start
+    //------------------------------
+    float frameHeightStart = (float)startButton.height/NUM_FRAMES; //390
+    Rectangle sourceRecStart = {0,0, (float)startButton.width, frameHeightStart};
+
+    Rectangle startBottonBounds = { screenWidth/2.0f - startButton.width/2.0f, screenHeight/2.0f - startButton.height/NUM_FRAMES/2.0f,
+                            (float)startButton.width, frameHeightStart};
+    //------------------------------
+
+    //Parametros del boton Settings
+    //------------------------------
+    float frameHeightSettings = (float)settingsButton.height/NUM_FRAMES; //390
+    Rectangle sourceRecSettings = {0,0, (float)settingsButton.width, frameHeightSettings};
+
+    Rectangle settingButtonBounds = { screenWidth/10.0f - settingsButton.width/2.0f, screenHeight/8.0f - settingsButton.height/NUM_FRAMES/2.0f,
+                            (float)settingsButton.width, frameHeightSettings};
+    //------------------------------
+
+    //Variables que voy a usar
+    int startButtonState = 0;
+    bool startButtonAction = false;
+
+    Vector2 mousePoint = {0.0f, 0.0f};
+
+    SetTargetFPS(60);
 
     //	Create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -45,14 +99,63 @@ int main(int argc, const char * argv[])
     char buf[4096];
     string userInput;
 
-    do {
-        game->handleEvents();
-        game->update();
-        game->render();
+    //Main loop del juego y cliente
+    while(!WindowShouldClose()) {
+
+        //Update
+        //-------------
+        mousePoint = GetMousePosition();
+        startButtonAction = false;
+
+
+        //-------------------------
+        if (CheckCollisionPointRec(mousePoint, startBottonBounds)) {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                startButtonState = 2 ;
+            }
+            else {
+                startButtonState = 1;
+            }
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+                startButtonAction = true;
+            }
+        }
+        else {
+            startButtonState = 1;
+        }
+        if (startButtonAction) {
+            string message = "Test Miedo";
+            int sendRes = send(sock, message.c_str(), message.size() + 1, 0);
+            if (sendRes == -1)
+            {
+                cout << "Could not send to server! Whoops!\r\n";
+                continue;
+            }
+            PlaySound(fxButton);
+            CloseWindow();
+
+            InitWindow(screenWidth, screenHeight, "Lamento en el Tibet");
+            BeginDrawing();
+            //ClearBackground(WHITE);
+            DrawTexture(backgroundImage, 0,0, WHITE);
+            EndDrawing();
+        }
+
+        sourceRecStart.y = startButtonState* frameHeightStart;
+
+        //Draw
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawTexture(backgroundImage, 0,0, WHITE);
+        DrawTextureRec(startButton, sourceRecStart, (Vector2){ startBottonBounds.x, startBottonBounds.y }, WHITE); // Draw button frame
+        DrawTextureRec(settingsButton, sourceRecSettings, (Vector2){settingButtonBounds.x, settingButtonBounds.y}, WHITE);
+
+        EndDrawing();
 
         //		Enter lines of text
-        cout << "> ";
-        getline(cin, userInput);
+
 
         //		Send to server
         int sendRes = send(sock, userInput.c_str(), userInput.size() + 1, 0);
@@ -76,9 +179,14 @@ int main(int argc, const char * argv[])
             //		Display response
             cout << string(buf, 0, bytesReceived) << "\r\n";
         }
-    } while(game->running());
+    };
+    UnloadTexture(startButton);  // Unload button texture
+    UnloadSound(fxButton);  // Unload sound
+
+    CloseAudioDevice();     // Close audio device
+
+    CloseWindow();
     close(sock);
 
-    game->clean();
 
 }
