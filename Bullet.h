484ad7a3cf08a68b1f164a742608a4e5417322ba
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2023-04-12 14:39:37 -0600

    3.0

--- Bullet.h
+++ Bullet.h
@@ -10,18 +10,25 @@
 class Bullet : public Entity {
 public:
     Bullet(raylib::Texture* texture, raylib::Rectangle inClip, raylib::Rectangle outClip, float speed, int type);
+    ~Bullet();
     void Update();
     bool IsHit();
     void Reset(raylib::Rectangle outClip);
     void setHit();
     raylib::Rectangle getOutClip();
     void setOutClip(raylib::Rectangle outClipNew);
+    bool getRecycled() const;
+    Bullet* getNextPtr() const;
+    void setNextPtr(Bullet* next);
+    void setRecycled(bool nCondition);
+    Bullet* next;
 
 
 private:
     float speed;
     bool hit;
     int type;
+    bool recycled;
 };
 
 
